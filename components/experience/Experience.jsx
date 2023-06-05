3b91bcc48f398172f58e0be011de0c55105619e1
Abdul kadar khan <95237480+Akprogramm@users.noreply.github.com>
2023-06-05 21:16:16 +0530

    Add files via upload

---
+++ components/experience/Experience.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BsFillPatchCheckFill } from 'react-icons/bs';
+import './experience.css';
+
+const Experience = () => {
+  return (
+    <section id="experience">
+      <h2>The Skills I Have</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <div className="experience__content">
+          <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>HTML</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>CSS</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>JavaScript</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>React js , Redux</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Node js,Express js</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>MongoDB</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Experience
